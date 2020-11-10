9c2c0ee14ece51ab07a050cb0ef611b2ddaf3fab
Roman <romagritcaenko@gmail.com>
2020-11-10 20:55:15 +0200

    change fix

--- 01-homework/01hw.js
+++ 01-homework/01hw.js
@@ -8,7 +8,7 @@ console.log('Суммарная стоимость товаров: ' + (a + b + 
 console.log('Суммарная стоимость без копеек: ' + (Math.trunc(a) + Math.trunc(b) + Math.trunc(c)));
 console.log('Округлённая стоимость: ' + Math.round((a + b + c) / 100 ) * 100);
 console.log('Сумма всех товаров - чётное число: ' + (Math.floor(a + b + c)%2 == 0));
-console.log('Сдача при оплате 500 грн: ' + (500 - (a + b + c)));
+console.log('Сдача при оплате 500 грн: ' + (500 - (a + b + c)).toFixed(2));
 console.log('Среднее значение цен: ' + ((a + b + c)/3).toFixed(2));
 console.log(`Скидка: ${randomDiscount}%`);
 console.log('Сумма со скидкой: ' + (((a + b + c)-((a + b + c) / 100 * randomDiscount)).toFixed(2)));
@@ -22,7 +22,7 @@ console.log(`Максимальная цена: ${Math.max(a, b, c)}
 Суммарная стоимость без копеек: ${Math.trunc(a) + Math.trunc(b) + Math.trunc(c)}
 Округлённая стоимость: ${Math.round((a + b + c) / 100 ) * 100}
 Сумма всех товаров - чётное число: ${Math.floor(a + b + c)%2 == 0}
-Сдача при оплате 500 грн: ${500 - (a + b + c)}
+Сдача при оплате 500 грн: ${500 - (a + b + c).toFixed(2)}
 Среднее значение цен: ${((a + b + c)/3).toFixed(2)}
 Скидка: ${randomDiscount}%
 Сумма со скидкой: ${((a + b + c)-((a + b + c) / 100 * randomDiscount)).toFixed(2)}
