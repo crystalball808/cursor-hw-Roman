79d052fc225ec953615ca30ecb1d8d77d07c784e
Roman <romagritcaenko@gmail.com>
2020-11-10 20:45:42 +0200

    fix

--- 01-homework/01hw.js
+++ 01-homework/01hw.js
@@ -1,6 +1,7 @@
 const a = 15.678;
 const b = 123.965;
 const c = 90.2345;
+const randomDiscount = Math.trunc(Math.random(0,1)*100);
 console.log('Максимальная цена: ' + Math.max(a, b, c));
 console.log('Минимальная цена: ' + Math.min(a, b, c));
 console.log('Суммарная стоимость товаров: ' + (a + b + c));
@@ -9,7 +10,6 @@ console.log('Округлённая стоимость: ' + Math.round((a + b + 
 console.log('Сумма всех товаров - чётное число: ' + (Math.floor(a + b + c)%2 == 0));
 console.log('Сдача при оплате 500 грн: ' + (500 - (a + b + c)));
 console.log('Среднее значение цен: ' + ((a + b + c)/3).toFixed(2));
-const randomDiscount = Math.trunc(Math.random(0,1)*100);
 console.log(`Скидка: ${randomDiscount}%`);
 console.log('Сумма со скидкой: ' + (((a + b + c)-((a + b + c) / 100 * randomDiscount)).toFixed(2)));
 console.log('Чистая прибыль: ' + ((a + b + c) / 2 -((a + b + c) / 100 * randomDiscount)).toFixed(2));
