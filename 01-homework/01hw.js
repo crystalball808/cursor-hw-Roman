306cef3ea918702118032410f1f44371f780f958
Roman <romagritcaenko@gmail.com>
2020-11-11 11:23:58 +0200

    refactored

--- 01-homework/01hw.js
+++ 01-homework/01hw.js
@@ -1,29 +1,42 @@
-const a = 15.678;
-const b = 123.965;
-const c = 90.2345;
+const pricePen = 15.678;
+const priceBook = 123.965;
+const priceRuler = 90.2345;
+const cash = 500;
 const randomDiscount = Math.trunc(Math.random(0,1)*100);
-console.log('Максимальная цена: ' + Math.max(a, b, c));
-console.log('Минимальная цена: ' + Math.min(a, b, c));
-console.log('Суммарная стоимость товаров: ' + (a + b + c));
-console.log('Суммарная стоимость без копеек: ' + (Math.trunc(a) + Math.trunc(b) + Math.trunc(c)));
-console.log('Округлённая стоимость: ' + Math.round((a + b + c) / 100 ) * 100);
-console.log('Сумма всех товаров - чётное число: ' + (Math.floor(a + b + c)%2 == 0));
-console.log('Сдача при оплате 500 грн: ' + (500 - (a + b + c)).toFixed(2));
-console.log('Среднее значение цен: ' + ((a + b + c)/3).toFixed(2));
-console.log(`Скидка: ${randomDiscount}%`);
-console.log('Сумма со скидкой: ' + (((a + b + c)-((a + b + c) / 100 * randomDiscount)).toFixed(2)));
-console.log('Чистая прибыль: ' + ((a + b + c) / 2 -((a + b + c) / 100 * randomDiscount)).toFixed(2));
+const maxPrice = Math.max(pricePen, priceRuler, priceBook);
+const minPrice = Math.min(pricePen, priceRuler, priceBook);
+const totalCost = pricePen + priceRuler + priceBook;
+const truncTotalCost = Math.trunc(pricePen) + Math.trunc(priceBook) + Math.trunc(priceRuler);
+const roundedCost = Math.round(totalCost / 100 ) * 100;
+const isEven = Math.floor(totalCost)%2 == 0;
+const cashChange = Number((cash - totalCost).toFixed(2));
+const average = Number((totalCost/3).toFixed(2));
+const discAmount = totalCost / 100 * randomDiscount;
+const costDiscount = Number((totalCost - discAmount).toFixed(2));
+const netProfit = Number((totalCost / 2 - discAmount).toFixed(2));
+
+console.log('Максимальная цена: ' + maxPrice);
+console.log('Минимальная цена: ' + minPrice);
+console.log('Суммарная стоимость товаров: ' + totalCost);
+console.log('Суммарная стоимость без копеек: ' + truncTotalCost);
+console.log('Округлённая стоимость: ' + roundedCost);
+console.log('Сумма всех товаров - чётное число: ' + isEven);
+console.log('Сдача при оплате 500 грн: ' + cashChange);
+console.log('Среднее значение цен: ' + average);
+console.log('Скидка: ' + randomDiscount + '%');
+console.log('Сумма со скидкой: ' + costDiscount);
+console.log('Чистая прибыль: ' + netProfit);
 
 console.log('Advanced');
 
-console.log(`Максимальная цена: ${Math.max(a, b, c)}
-Минимальная цена: ${Math.min(a, b, c)}
-Суммарная стоимость товаров: ${a + b + c}
-Суммарная стоимость без копеек: ${Math.trunc(a) + Math.trunc(b) + Math.trunc(c)}
-Округлённая стоимость: ${Math.round((a + b + c) / 100 ) * 100}
-Сумма всех товаров - чётное число: ${Math.floor(a + b + c)%2 == 0}
-Сдача при оплате 500 грн: ${500 - (a + b + c).toFixed(2)}
-Среднее значение цен: ${((a + b + c)/3).toFixed(2)}
+console.log(`Максимальная цена: ${maxPrice}
+Минимальная цена: ${minPrice}
+Суммарная стоимость товаров: ${totalCost}
+Суммарная стоимость без копеек: ${truncTotalCost}
+Округлённая стоимость: ${roundedCost}
+Сумма всех товаров - чётное число: ${isEven}
+Сдача при оплате 500 грн: ${cashChange}
+Среднее значение цен: ${average}
 Скидка: ${randomDiscount}%
-Сумма со скидкой: ${((a + b + c)-((a + b + c) / 100 * randomDiscount)).toFixed(2)}
-Чистая прибыль: ${((a + b + c) / 2 -((a + b + c) / 100 * randomDiscount)).toFixed(2)}`);
+Сумма со скидкой: ${costDiscount}
+Чистая прибыль: ${netProfit}`);
