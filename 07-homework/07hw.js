86f67d080e0ea448a7d16eef6699f1222aede845
Roman <romagritcaenko@gmail.com>
2020-12-01 20:35:48 +0200

    07hw done

---
+++ 07-homework/07hw.js
@@ -0,0 +1,37 @@
+const ukraine = { tax: 0.195, middleSalary: 1789, vacancies: 11476 };
+const latvia = { tax: 0.25, middleSalary: 1586, vacancies: 3921 };
+const litva = { tax: 0.15, middleSalary: 1509, vacancies: 1114 };
+
+const getMyTaxes = function(salary){
+	return +(this.tax * salary).toFixed(2);
+}
+
+console.log(getMyTaxes.call(ukraine,9999));
+
+const getMiddleTaxes = function() {
+	return +(this.tax * this.middleSalary).toFixed(2);
+}
+
+console.log(getMiddleTaxes.call(litva));
+
+const getTotalTaxes = function() {
+	return +(this.tax * this.middleSalary * this.vacancies).toFixed(2);
+}
+
+console.log(getTotalTaxes.call(latvia));
+
+const getMySalary = function(country) {
+	setInterval(()=>{
+		const salary =  Math.floor(Math.random() * (2000 - 1500 + 1)) + 1500;
+		const taxes = +(country.tax * salary).toFixed(2);
+		const profit = +(salary - taxes).toFixed(2);
+		;
+		console.log({
+			salary,
+			taxes,
+			profit
+		});
+	},10000)
+}
+
+getMySalary(ukraine);
