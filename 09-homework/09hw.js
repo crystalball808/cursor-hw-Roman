c0caaf7f8a98fafebfc60f4a36d22bec4e96ae33
Roman <romagritcaenko@gmail.com>
2020-12-08 13:10:45 +0200

    09hw fix

--- 09-homework/09hw.js
+++ 09-homework/09hw.js
@@ -1,10 +1,11 @@
-const createKvadratiki= function(){
+const createSquares= function(){
 	document.body.childNodes[2].remove();
 	const canvas =  document.createElement('div');
 	canvas.style.display = 'table';
-	for(let i = 0; i<5;i++){
+	const canvasSize = 5;
+	for(let i = 0; i<canvasSize;i++){
 		const row = document.createElement('div');
-		for(let i = 0; i<5;i++){
+		for(let i = 0; i<canvasSize;i++){
 		const square = document.createElement('div');
 		const randomColor = '#' + (Math.random().toString(16) + '000000').substring(2,8).toUpperCase();
 	    square.style.background = randomColor;
@@ -18,4 +19,4 @@ const createKvadratiki= function(){
 	document.body.append(canvas);
 }
 
-setInterval(createKvadratiki,1000);
+setInterval(createSquares,1000);
